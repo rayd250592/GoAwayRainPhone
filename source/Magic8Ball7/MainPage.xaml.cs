1aec7be4614ca9db27e62b68b1e7bf1835cada91
rayd25051992 <rayd_250592@outlook.com>
2014-01-18 23:17:23 +0000

    submitted

---
+++ source/Magic8Ball7/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Magic8Ball7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (question.Text == "")
+            {
+
+                response.Text = "You must ask me a question first";
+
+            }
+
+            else
+            {
+
+
+
+                string[] responses = { "It is certain", "It is decidedly so", "Without a doubt", "Yes", "Nahhhh",
+                                     "No way", "You serious!?", "Yes you may rely on it", "Outlook good", "Ask again later", "Cannot predict now",
+                                 "That could work"};
+
+
+                response.Text = responses[new Random().Next(0, responses.Length)];
+
+
+            }
+        }
+    }
+}
